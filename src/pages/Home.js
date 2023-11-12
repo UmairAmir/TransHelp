d150bb2f8bf50ffdc884d19df4a2235716d80b89
Umair Amir <umair.amir19999@gmail.com>
2023-11-12 05:04:31 +0500

    UI changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import Footer from "../components/footer.js"
 import Carousel from "../components/Carousel.js"
 import diversity from "../assets/diversity.png"
 import partner1 from "../assets/partner1.png"
+import man1 from "../assets/man1.jpg"
+import man2 from "../assets/man2.jpg"
 
 
 const Home = () => {
@@ -16,12 +18,12 @@ const Home = () => {
     {
       heading: 'This industry',
       paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      image: 'https://cdn.pixabay.com/photo/2023/11/04/07/57/owl-8364426_1280.jpg',
+      image: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
     {
       heading: 'That industry',
       paragraph: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      image: 'https://cdn.pixabay.com/photo/2023/10/26/18/18/coneflower-8343278_640.jpg',
+      image: 'https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
     // Add more slides as needed
   ];
@@ -46,39 +48,8 @@ const Home = () => {
       </div>
 
 
-      {/* <section className={styles.whyUs}>
-        <div className={styles.allCards}>
-            <div className={styles.first}>
-                <Card
-                    heading="Inclusive Care for Marginalized Communities"
-                    paragraph="We understand the unique challenges and needs of transgender individuals, women, and those living with HIV/AIDS/STIs. Our team of experienced and culturally sensitive healthcare providers, mental health professionals, and legal experts are here to support you on your journey to wellness."
-                    />
-                <Card
-                    heading="Convenient Online Consultations"
-                    paragraph="Say goodbye to long wait times and travel hassles. TransHelp offers secure and user-friendly online consultations, allowing you to access the care you need from the comfort and privacy of your own home. Our telehealth platform is accessible through computers, smartphones, and tablets, making healthcare, mental health support, and legal guidance just a few clicks away."
-                    />
-                <Card
-                    heading="Expert Doctors, Psychologists, and Lawyers"
-                    paragraph="Our network of doctors specializing in sexual health, psychologists trained in mental health support, and lawyers well-versed in legal matters ensure that you receive professional, personalized, and evidence-based care. We prioritize the highest standards of expertise and professionalism to provide you with the best possible support."
-                    />
-            </div>
-            <div className={styles.last}>
-                <Card
-                    heading="Confidentiality and Privacy"
-                    paragraph="Your privacy is of utmost importance to us. TransHelp employs stringent security measures to protect your personal information and ensure all consultations are confidential. Our platform utilizes encrypted communication channels, safeguarding your privacy throughout the entire process."
-                    />
-                <Card
-                    heading="Empowering Resources and Education"
-                    paragraph="TransHelp is more than just a telehealth app. We are dedicated to empowering our users with knowledge and resources. We offer a wealth of educational materials, articles, and frequently asked questions, providing you with the information you need to make informed decisions about your sexual health, mental well-being, and legal matters."
-                    />
-                <Card
-                    heading="Multilingual Support"
-                    paragraph="We value diversity and understand that language and cultural barriers can affect access to care. That's why TransHelp provides culturally competent support and offers multilingual services to cater to our diverse community. We strive to ensure that language is never a barrier to receiving the care you deserve."
-                    />
-            </div>
-        </div>
-      </section> */}
-      <section>
+      <section className={styles.whyUs}>
+        <h1 className={styles.whyUsh1}>Our telehealth app connects individuals with trusted doctors, psychologists, and lawyers through convenient online consultations.</h1>
         <div className={styles.cards}>
           <div className={styles.first2}>
             <Card2 heading="Inclusive Care for Marginalized Communities" para="We understand the unique challenges and needs of transgender individuals, women, and those living with HIV/AIDS/STIs. Our team of experienced and culturally sensitive healthcare providers, mental health professionals, and legal experts are here to support you on your journey to wellness." icon="globe"/>
